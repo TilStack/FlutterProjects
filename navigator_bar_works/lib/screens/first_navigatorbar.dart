3b84b88f82c1fe6a26f8236de3b6b0a3ad84300f
TilStack <israel01tientcheu@gmail.com>
2024-06-29 21:36:58 +0100

    some modification in folder. And initialisation of spine botton navigator bar

--- navigator_bar_works/lib/screens/first_navigatorbar.dart
+++ navigator_bar_works/lib/screens/first_navigatorbar.dart
@@ -2,10 +2,9 @@ import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:navigator_bar_works/utils/constants.dart';
-import 'package:navigator_bar_works/widgets/card_contain.dart';
-import 'package:navigator_bar_works/widgets/grid_card.dart';
+import 'package:navigator_bar_works/widgets/FirstOne/card_contain.dart';
+import 'package:navigator_bar_works/widgets/FirstOne/grid_card.dart';
 import 'package:zoom_tap_animation/zoom_tap_animation.dart';
-
 
 class FirstNavigatorBar extends StatefulWidget {
   const FirstNavigatorBar({super.key});
@@ -30,97 +29,109 @@ class _FirstNavigatorBarState extends State<FirstNavigatorBar> {
         children: [
           SingleChildScrollView(
             child: Column(
-                children: [
-                    Container(
-                      margin: const EdgeInsets.only(top: 20,right: 10,left: 10, bottom: 15),
-                      child: const CardContent(title: 'My First Title', subtitle: 'The name given to a book, movie, song, or any other creative work. ', image: 'assets/png/till.png',),
-                    ),
-                  Container(
-                    width: size.width,
-                    height: 2,
-                    color: SECOND_COLOR.withOpacity(0.5),
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(
+                      top: 20, right: 10, left: 10, bottom: 15),
+                  child: const CardContent(
+                    title: 'My First Title',
+                    subtitle:
+                        'The name given to a book, movie, song, or any other creative work. ',
+                    image: 'assets/png/till.png',
                   ),
-                  Container(
-                    width: size.width,
-                    padding: const EdgeInsets.all(10.0),
-                    child: GridView.builder(
-                        physics: const NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount( crossAxisCount: 2,
-                          crossAxisSpacing: 10.0,
-                          mainAxisSpacing: 10.0,
-                        ),
-                        itemBuilder: (BuildContext context, int index) {
-                          final item = ITEMS[index];
-                          return GridCard(icon: item['icon'],title: item['title'],subtitle: item['subtitle'],);
-                          },
-                        itemCount: ITEMS.length
-                    ),
-                  ),
-                  SizedBox(height: 80),
-                ],
-              ),
+                ),
+                Container(
+                  width: size.width,
+                  height: 2,
+                  color: SECOND_COLOR.withOpacity(0.5),
+                ),
+                Container(
+                  width: size.width,
+                  padding: const EdgeInsets.all(10.0),
+                  child: GridView.builder(
+                      physics: const NeverScrollableScrollPhysics(),
+                      shrinkWrap: true,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 10.0,
+                        mainAxisSpacing: 10.0,
+                      ),
+                      itemBuilder: (BuildContext context, int index) {
+                        final item = ITEMS[index];
+                        return GridCard(
+                          icon: item['icon'],
+                          title: item['title'],
+                          subtitle: item['subtitle'],
+                        );
+                      },
+                      itemCount: ITEMS.length),
+                ),
+                SizedBox(height: 80),
+              ],
             ),
+          ),
 
           // Custum navigation Bar
           Positioned(
-    bottom: 18,
-    left: 18,
-    right: 18,
-    height: 86
-    ,child: Container(
-            decoration: BoxDecoration(
-              border: Border.all(width: 2,color: theme.scaffoldBackgroundColor),
-              borderRadius: const BorderRadius.only(
-                topRight: Radius.circular(24),
-                topLeft: Radius.circular(24),
-                bottomLeft: Radius.circular(52),
-                bottomRight: Radius.circular(52)
-              ),
-              color: theme.scaffoldBackgroundColor.withOpacity(0.1),
-
-            ),
-            child: ClipRect(
-              child: ClipPath(
-                clipper: MyClipper(),
-                child: BackdropFilter(filter: ImageFilter.blur(sigmaX: 8,sigmaY: 8),
-                child: Container(),),
-              ),
-            ),
-          )),
+              bottom: 18,
+              left: 18,
+              right: 18,
+              height: 86,
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                      width: 2, color: theme.scaffoldBackgroundColor),
+                  borderRadius: const BorderRadius.only(
+                      topRight: Radius.circular(24),
+                      topLeft: Radius.circular(24),
+                      bottomLeft: Radius.circular(52),
+                      bottomRight: Radius.circular(52)),
+                  color: theme.scaffoldBackgroundColor.withOpacity(0.1),
+                ),
+                child: ClipRect(
+                  child: ClipPath(
+                    clipper: MyClipper(),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 8, sigmaY: 8),
+                      child: Container(),
+                    ),
+                  ),
+                ),
+              )),
 
           // Button Navigation Bar item
           Positioned(
-            bottom: 18,
-            left: 22,
-            right: 22,
-            height: 86
-          ,child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            mainAxisSize: MainAxisSize.max
-          ,children: [
-            BuildItem(Icons.home_outlined,0),
-            BuildItem(Icons.apps,1),
-            BuildItem(Icons.notifications_active_rounded,2),
-            BuildItem(Icons.settings_outlined,3),
-          ],)),
+              bottom: 18,
+              left: 22,
+              right: 22,
+              height: 86,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  BuildItem(Icons.home_outlined, 0),
+                  BuildItem(Icons.apps, 1),
+                  BuildItem(Icons.notifications_active_rounded, 2),
+                  BuildItem(Icons.settings_outlined, 3),
+                ],
+              )),
         ],
       ),
     );
   }
-  Widget BuildItem(IconData icon, index){
+
+  Widget BuildItem(IconData icon, index) {
     return ZoomTapAnimation(
-      onTap: (){
-          setState(() {
-            selectedItem = index;
-          });
+      onTap: () {
+        setState(() {
+          selectedItem = index;
+        });
       },
-      child: Icon(
-          icon,
-          color: selectedItem == index? SECOND_COLOR: Colors.yellow,
-          size: selectedItem == index ? 35 :  30
-      ),
+      child: Icon(icon,
+          color: selectedItem == index ? SECOND_COLOR : Colors.yellow,
+          size: selectedItem == index ? 35 : 30),
     );
   }
 }
