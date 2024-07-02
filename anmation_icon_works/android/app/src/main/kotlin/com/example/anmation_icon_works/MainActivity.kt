71b3c0db7bd6ea8a8a6479963bf38acad7c0c2f5
TilStack <israel01tientcheu@gmail.com>
2024-07-02 15:41:38 +0100

    created projects for icon and page transition.

---
+++ anmation_icon_works/android/app/src/main/kotlin/com/example/anmation_icon_works/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.anmation_icon_works
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ transition_works/android/app/src/main/kotlin/com/example/transition_works/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.transition_works
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
